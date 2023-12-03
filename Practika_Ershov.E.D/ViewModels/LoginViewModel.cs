79ae48700f3be2d94d839a590ca6ee5a61cc5544
Евгений Ершов <evgen6538655@gmail.com>
2023-12-03 14:51:20 +0800

    -Добавил папку View , в которой присутвует начальное  окно авторизации  LoginView.xaml -В LoginView сделал первое,тестовое оформление окна авторизации -Также добавил папку CustomControls , в которой присутвует поле для ввода пароля(в самом определении этого поля задали свойсва защищенности благодаря SecureString)

--- Practika_Ershov.E.D/ViewModels/LoginViewModel.cs
+++ Practika_Ershov.E.D/ViewModels/LoginViewModel.cs
@@ -1,12 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Practika_Ershov.E.D.ViewModels
 {
-    internal class LoginViewModel
+    public class LoginViewModel : ViewModelBase
     {
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisable = true;
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisable 
+        {
+            get
+            {
+               return _isViewVisable;
+            }
+            set
+            {
+                _isViewVisable = value;
+                OnPropertyChanged(nameof(IsViewVisable));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPaasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new ViewModelCommand(p =>ExecuteRecoverPassCommand("",""));
+        }
+
+    
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || Password == null ||
+                Password.Length < 3)
+                validData = false;
+            else 
+                validData = true;
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        private void ExecuteRecoverPassCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
